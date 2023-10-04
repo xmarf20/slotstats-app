3ffb320dd6de5198c19f53c4458d42332fefdd2b
xmarf20 <144128572+xmarf20@users.noreply.github.com>
2023-10-04 11:23:55 +0000

    Removed App

--- slotstats-app/src/app/pages/bonus-hunt/bonus-hunt.component.ts
+++
@@ -1,8 +0,0 @@
-import {Component} from '@angular/core';
-@Component({
-  selector: 'app-bonus-hunt',
-  templateUrl: './bonus-hunt.component.html'
-})
-export class BonusHuntComponent {
-
-}
--- slotstats-app/src/app/pages/slots-list/slots-list.component.ts
+++
@@ -1,8 +0,0 @@
-import {Component} from '@angular/core';
-
-@Component({
-  selector: 'slots-list',
-  templateUrl: './slots-list.component.html'
-})
-export class SlotsListComponent {
-}
